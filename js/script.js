7516fcbc583d468aa2e86ce7b475cc138863484d
Liem Phan <minhliemphp@gmail.com>
2016-12-19 15:56:58 +0700

    update final

--- js/script.js
+++ js/script.js
@@ -33,5 +33,24 @@ $(document).ready(function(){
 			sub.stop().slideUp();
 		})
 	}
-
 })
+
+function animation(classname, animate){
+	classname  = classname || 'animation';
+
+	$('.'+classname).each(function(index,el){
+		new Waypoint({
+			element: el,
+			handler:function(direction){
+				var element = $(this.element),
+				delay = element.attr("data-delay");
+				// $(el).removeClass('hide-ani');	
+				setTimeout(function(){
+					$(el).addClass('animated '+animate+' show-ani');
+				},delay);
+				this.destroy();
+			},
+			offset: '60%'
+		});
+	});
+}
